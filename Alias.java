4abacbdc8e495ed11e2666f54f5d6536d3e20fd5
zeinab <zeinabsalifou22@gmail.com>
2025-01-18 18:37:41 -0500

    Delete Alias.java

--- Alias.java
+++
@@ -1,7 +0,0 @@
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-// Cette annotation permet d'associer un alias à un champ ou à une méthode.
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Alias {
-    String value() default "";
-}
