e0132dcffbf8c44302b79b205d8470e370ca6c2a
zeinab <zeinabsalifou22@gmail.com>
2025-01-18 18:40:43 -0500

    Delete JsonProcessingException.java

--- JsonProcessingException.java
+++
@@ -1,8 +0,0 @@
-// La classe JsonProcessingException étend RuntimeException et permet de gérer des exceptions spécifiques
-public class JsonProcessingException extends RuntimeException {
-
-    // constructeur
-    public JsonProcessingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
