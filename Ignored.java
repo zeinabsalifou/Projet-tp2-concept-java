f43290f8720572cf8cb84a79208eca4d2b490cb2
zeinab <zeinabsalifou22@gmail.com>
2025-01-18 18:39:59 -0500

    Delete Ignored.java

--- Ignored.java
+++
@@ -1,6 +0,0 @@
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-// Annotation utilisée pour marquer les champs d'une classe qui doivent être ignorés
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Ignored {
-}
